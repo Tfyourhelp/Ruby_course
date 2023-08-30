dd9ac84d85ccfd5cd151579a559f1818864b44ad
Bảo Duy <bduy2015@gmail.com>
2023-08-30 16:49:52 +0700

    Bài tập IO,CSV,JSON

---
+++ ruby/lesson_7/io_csv_json/CSV.rb
@@ -0,0 +1,16 @@
+require 'csv'
+CSV.open('contact.csv', 'w') do |csv|
+  csv << ['Name', 'Email', 'Phone']
+  csv << ['John', 'john@example.com', '555-1234']
+  csv << ['Alice', 'alice@example', '555-5678']
+  csv << ['Bob', 'bob@example.com', '555-9876']
+end
+
+
+data = CSV.read('contact.csv', headers: true)
+data.each do |row|
+  print "Name: #{row['Name']}, Email: #{row['Email']}, Phone: #{row['Phone']}"
+  print "\n"
+end
+
+    

daadfef8abc91225abc99b04870a5bfc205fffd1
I am Duy <bduy2015@gmail.com>
2023-09-08 16:14:37 +0700

    bài tập code smell 2 đã sửa theo anh Ngọc

--- ruby/lesson_9/code_smell_2/baitap_codesmell.rb
+++ ruby/lesson_9/code_smell_2/baitap_codesmell.rb
@@ -1,17 +1,13 @@
 class OrderProcessor
 
   def process_order(order)
-    customer = order.customer
-    items = order.items
 
-
-    order.existence_check()
-    total_price = order.total_order_value()
+    total_price = order.existence_check_and_calculate_price
     total_price = order.discount_apply(total_price)
     # Tạo hóa đơn
-    invoice = create_invoice(customer, items, total_price)
+    invoice = create_invoice(customer = order.customer, items = order.items, total_price)
     # Gửi email xác nhận
-    send_confirmation_email(customer, invoice)
+    send_confirmation_email(customer = order.customer, invoice)
 
   end
 
@@ -28,36 +24,32 @@ class OrderProcessor
   end
 end
 
-
 class Order
   attr_accessor :customer, :items
+
   def initialize(customer, items)
     @customer = customer
     @items = items
   end
 
   # Kiểm tra có mặt hàng hay không
-  def existence_check()
+  def existence_check_and_calculate_price
+    
     raise "Không có mặt hàng nào!" if @items.empty?
     # Kiểm tra tồn kho
-    @items.each do |item|
-      item.item_inventory_check()
-    end
-  end
-
-  # Tính tổng giá trị đơn hàng
-  def total_order_value()
     total_price = 0
     @items.each do |item|
-      total_price += item.price * item.quantity
+      item.item_inventory_check
+      # Tính tổng giá trị đơn hàng
+      total_price = item.calculate_total_price
     end
-    return total_price
+    total_price
   end
 
   # Áp dụng giảm giá nếu có
   def discount_apply(total_price)
     total_price -= total_price * @customer.discount_percent if @customer.has_discount?
-    return total_price
+    total_price
   end
 end
 
@@ -85,8 +77,11 @@ class Item
   end
 
   # Kiểm tra tồn kho
-  def item_inventory_check()
+  def item_inventory_check
     raise "Không đủ hàng trong kho cho #{@name}" if @available_quantity < @quantity
   end
 
+  def calculate_total_price
+    total_price += @price + @quantity
+  end
 end

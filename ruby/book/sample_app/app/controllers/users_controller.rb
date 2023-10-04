5f16c6d03fa82e7ca78c705e87cb4fe6ba2613b8
I am Duy <bduy2015@gmail.com>
2023-10-04 18:18:07 +0700

    chương 10

--- ruby/book/sample_app/app/controllers/users_controller.rb
+++ ruby/book/sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index,:edit, :update, :destroy]
+# trang user/:id ai cũn vào dc(chưa đăng nhập hay đăng nhập r)
+# bị 1 vấn đề  trước khi làm ch10 , đó là đăng nhập tk1 xong sửa path thành user/2/edit --> thì edit dc tk2 --> sửa : tk1 đăng nhập vào users/2/edit thì sẽ bị redirect_to(root_user) (corected_user)
+# còn nút setting phía bên trên thì auto vào edit của user của tk đăng nhập bởi vì đường link edit_user_path(current_user = user.id(id của tk đnhập))
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+  before_action :find_id, only: [:show, :edit, :update]
+
+  def index
+    @users =User.page(params[:page])  #.per(5) 
+  end
+
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
   
   def new
@@ -16,12 +28,65 @@ class UsersController < ApplicationController
       redirect_to @user # (user_url(@user))
     else
       render 'new' # new la action new trong cung controller (User) 
+# --> render tiếp new.html.erb --> hiện ra lỗi nếu form trc bị lỗi và hiện ra form để điền lại
     end
+  end
+
+  def edit
+    # @user = User.find(params[:id])
+  end
+
+  def update
+    # @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+# render edit la action edit(quan trọng là view/users/edit) trong cung controller (User) 
+# --> render tiếp edit.html.erb --> hiện ra lỗi nếu form trc bị lỗi và hiện ra form để điền lại    
+    end
+  end
+
+  def destroy
+    adsfadf
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    fadfadf
+    redirect_to users_url
   end
 
   private 
   
+  def admin_user # trường hợp k phải hacker k phải admin nhưng hacker vẫn tìm cách delete dược
+    redirect_to(root_url) unless current_user.admin?
+  end
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in? # --> nếu chưa đăng nhập --> chưa có session[:user_id] --> current_user = nil
+      store_location
+# --> nếu chưa đăng nhập thì lưu get request , đăng nhập xong thì sẽ thực hiện gest request đó 
+      flash[:danger] = "Please log in"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user) #@user == current_user
+# nếu như truy cập vào trang user/:id/edit k dung user tương ứng thì nó sẽ trả về trang user/:id rồi bỏ qua hành động edit , update
+  end
+
+  def find_id
+    begin
+      @user = User.find(params[:id])
+    rescue
+      flash[:danger] = "User not found"
+      redirect_to root_path 
+    end 
   end
 end
--- ruby/book/sample_app/test/integration/users_signup_test.rb
+++ ruby/book/sample_app/test/integration/users_signup_test.rb
@@ -15,5 +15,21 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     # assert_select 'div#<CSS id for error explanation>'
     # assert_select 'div.<CSS class for field with error'
+
+    test "valid signup information" do
+      get signup_path
+      assert_difference "User.count", 1 do
+        post users_path, params: {  user: { name: "Example User",
+                                    email: "user@example.com",
+                                    password:          "password",
+                                    password_confirmation:"password"} }
+      end
+      follow_redirect!
+      assert_template "user/show"
+      # assert_not flash.FILL_IN
+      assert is_logged_in?
+    end
   end
+
+
 end
